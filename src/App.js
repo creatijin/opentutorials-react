bb8294e234ea37b7dc1ae870d773157680569b41
creatijin <newlex0124@gmail.com>
2019-05-03 02:36:37 +0900

    props + component file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Toc from "./components/Toc";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" subtxt="world wide web!"></Subject>
+        <Toc></Toc>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    )
+  }
 }
 
 export default App;
